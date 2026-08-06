c876bba7a1e2bdf7ea3f7884147cc3f73f10a75a
Sapienet <suchwawa@sapienet.com>
2026-08-06 12:37:48 +0900

    fix: Allow firebasestorage.googleapis.com in NextImage config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   serverExternalPackages: ['jose', 'jwks-rsa'],
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
