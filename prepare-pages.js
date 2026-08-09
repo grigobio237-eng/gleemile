09403001000c2be206848bc1f99e21299b70c534
Sapienet <suchwawa@sapienet.com>
2026-08-09 21:17:40 +0900

    fix: polyfill require to resolve Cloudflare Error 1101

--- prepare-pages.js
+++ prepare-pages.js
@@ -62,12 +62,51 @@ esbuild.build({
     'worker_threads', 'cluster', 'module'
   ],
   plugins: [mockPlugin],
+  banner: {
+    js: `
+import * as __node_crypto from 'node:crypto';
+import * as __node_fs from 'node:fs';
+import * as __node_path from 'node:path';
+import * as __node_url from 'node:url';
+import * as __node_vm from 'node:vm';
+import * as __node_child_process from 'node:child_process';
+import * as __node_stream from 'node:stream';
+import * as __node_util from 'node:util';
+import * as __node_assert from 'node:assert';
+import * as __node_os from 'node:os';
+import * as __node_querystring from 'node:querystring';
+import * as __node_https from 'node:https';
+import * as __node_http from 'node:http';
+import * as __node_zlib from 'node:zlib';
+import * as __node_events from 'node:events';
+import * as __node_buffer from 'node:buffer';
+import * as __node_async_hooks from 'node:async_hooks';
+
+const _nodeBuiltins = {
+  crypto: __node_crypto, fs: __node_fs, path: __node_path, url: __node_url, vm: __node_vm, child_process: __node_child_process, stream: __node_stream, util: __node_util, assert: __node_assert, os: __node_os, querystring: __node_querystring, https: __node_https, http: __node_http, zlib: __node_zlib, events: __node_events, buffer: __node_buffer, async_hooks: __node_async_hooks,
+  'node:crypto': __node_crypto, 'node:fs': __node_fs, 'node:path': __node_path, 'node:url': __node_url, 'node:vm': __node_vm, 'node:child_process': __node_child_process, 'node:stream': __node_stream, 'node:util': __node_util, 'node:assert': __node_assert, 'node:os': __node_os, 'node:querystring': __node_querystring, 'node:https': __node_https, 'node:http': __node_http, 'node:zlib': __node_zlib, 'node:events': __node_events, 'node:buffer': __node_buffer, 'node:async_hooks': __node_async_hooks
+};
+
+globalThis.require = function(id) {
+  if (_nodeBuiltins[id]) return _nodeBuiltins[id];
+  throw new Error('Not supported require: ' + id);
+};
+`
+  },
   define: {
     'process.env.NODE_ENV': '"production"'
   },
   metafile: true
 }).then(result => {
   fs.writeFileSync('bundle-meta.json', JSON.stringify(result.metafile));
+  
+  // Patch the esbuild __require stub to use our globalThis.require instead of throwing
+  let indexJsContent = fs.readFileSync(path.join(workerDir, 'index.js'), 'utf8');
+  indexJsContent = indexJsContent.replace(
+    /throw Error\(['"`]Dynamic require of ['"`] \+ x \+ ['"`] is not supported['"`]\);?/g,
+    'return globalThis.require(x);'
+  );
+  fs.writeFileSync(path.join(workerDir, 'index.js'), indexJsContent);
   
   console.log("Creating _routes.json...");
   const routes = {
